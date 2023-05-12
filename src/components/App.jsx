e7e0476024434909023d99ff6df9ee305c3d7fe9
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-05-12 19:31:08 +0200

    Add component Transaction History

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,11 @@ import { Section } from './Section/Section';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import user from './data/user.json';
 import data from './data/data.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export const App = () => {
   return (
@@ -23,6 +25,9 @@ export const App = () => {
       </Section>
       <Section title={'Task 3: Friends list'}>
         <FriendList friends={friends} />
+      </Section>
+      <Section title={'Task 4: Transaction history'}>
+        <TransactionHistory items={transactions} />
       </Section>
     </div>
   );
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,13 @@
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ item }) => {
+  return (
+    <thead>
+      <tr>
+        {/* <th>{type}</th>
+        <th>{amount}</th>
+        <th>{currency}</th> */}
+      </tr>
+    </thead>
+  );
+};
